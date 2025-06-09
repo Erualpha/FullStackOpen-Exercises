a614c340e62a3c1a7e858b101b73e419cd75b089
Erudite <92492556+Erualpha@users.noreply.github.com>
2025-06-10 01:03:33 +0800

    Solution par2/courseinfo/step10

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,3 +1,5 @@
+import Course from './components/Course'
+
 const App = () => {
   const courses = [
     {
@@ -43,38 +45,6 @@ const App = () => {
       ]
     }
   ]
-
-  const Course = ({ course }) => {
-
-    const Header = () => <h2>{course.name}</h2>
-
-    const Parts = () => (
-      <div>
-        {course.parts.map(part => 
-          <p key={part.id}>
-            {part.name} {part.exercises}
-          </p>
-        )}
-      </div>
-    )
-
-    const Total = () => {
-      const total = course.parts.reduce((sum, part) => sum + part.exercises, 0)
-      return (
-        <div>
-          <p><b>total of {total} exercises</b></p>
-        </div>
-      )
-    }
-
-    return (
-      <div>
-        <Header />
-        <Parts />
-        <Total />
-      </div>
-    )
-  }
 
   return (
     <div>
--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,45 +1,34 @@
-const Course = ( {course} ) => {
+const Course = ({ course }) => {
 
-    return (
-        <div>
-            <h1>{course.name}</h1>
-        </div>
+    const Header = () => <h2>{course.name}</h2>
+
+    const Parts = () => (
+      <div>
+        {course.parts.map(part => 
+          <p key={part.id}>
+            {part.name} {part.exercises}
+          </p>
+        )}
+      </div>
     )
 
-// const Header = () => {
+    const Total = () => {
+      const total = course.parts.reduce((sum, part) => sum + part.exercises, 0)
+      return (
+        <div>
+          <p><b>total of {total} exercises</b></p>
+        </div>
+      )
+    }
 
-//   return (
-//     <div>
-//       <h1>{course.name}</h1>
-//     </div>
-//   )
-// }
-
-// const Content = ({parts}) => {
-//   return(
-//     <div>
-//     <Part name={parts[0].name} exercises={parts[0].exercises} />
-//     <Part name={parts[1].name} exercises={parts[1].exercises} />
-//     <Part name={parts[2].name} exercises={parts[2].exercises} />
-//     </div>
-//   )
-// }
-
-// const Part = ({name,exercises}) => {
-//   return (
-//     <p>
-//       {name} {exercises}
-//     </p>
-//   )
-// }
-
-// const Total = ({parts}) => {
-//   return (
-//     <div>
-//     <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
-//     </div>
-//   )
-
+    return (
+      <div>
+        <Header />
+        <Parts />
+        <Total />
+      </div>
+    )
 }
 
 export default Course
+  

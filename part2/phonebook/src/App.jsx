3278276fba164641dae8de6be7b0dfcd65a97c38
Erudite <92492556+Erualpha@users.noreply.github.com>
2025-06-10 14:05:25 +0800

    Solution part2/phonebook/step3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,28 +2,34 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '040-123456',
+     }
   ]) 
-  const [newName, setNewName] = useState()
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
-    const exists = persons.filter(
+    const exists = persons.some(
       person => person.name.toLowerCase() === newName.toLowerCase()
-    ).length > 0
-
+    )
     if (exists) {
       alert(`${newName} is already added to phonebook`)
       return
     }
-
-    const nameObject = { name: newName }
+    const nameObject = { name: newName, number: newNumber }
     setPersons(persons.concat(nameObject))
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
 
@@ -33,20 +39,34 @@ const App = () => {
       <form onSubmit={addName}>
         <div>
           name: <input 
-          value={newName}
-          onChange={handleNameChange}
+            id="name-input"
+            name="name"
+            autoComplete="name"
+            value={newName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          number: <input
+            id="number-input"
+            name="number"
+            autoComplete="tel"
+            value={newNumber}
+            onChange={handleNumberChange}
           />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      <ul>
+      <div>
+        <h2>Numbers</h2>
         {persons.map((person, i) => 
-          <div key={person.name + i}>{person.name}</div>
+          <div key={person.name + i}>
+            {person.name} {person.number}
+          </div>
         )}
-      </ul>
+      </div>
     </div>
   )
 }

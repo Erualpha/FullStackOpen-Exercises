87c9f99a72e9dbf04efe0a142dcf532ea361ea10
Erudite <92492556+Erualpha@users.noreply.github.com>
2025-06-10 14:32:11 +0800

    Solution part2/phonebook/step4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '040-123456',
-     }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filterPerson, setFilterPerson] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
@@ -32,16 +34,39 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    setFilterPerson(event.target.value)
+  }
+
+  const filterResult = filterPerson
+    ? persons.filter(person =>
+        person.name.toLowerCase().includes(filterPerson.toLowerCase())
+      )
+    : persons;
 
   return (
     <div>
+
       <h2>Phonebook</h2>
+
+      <div>
+        filter shown with: <input
+          id="filter-input"
+          name="filter"
+          autoComplete='name'
+          value={filterPerson}
+          onChange={handleFilterChange}
+        />
+      </div>
+
+      <h2>add a new</h2>
+
       <form onSubmit={addName}>
         <div>
           name: <input 
             id="name-input"
             name="name"
-            autoComplete="name"
+            autoComplete='name'
             value={newName}
             onChange={handleNameChange}
           />
@@ -50,7 +75,7 @@ const App = () => {
           number: <input
             id="number-input"
             name="number"
-            autoComplete="tel"
+            autoComplete='number'
             value={newNumber}
             onChange={handleNumberChange}
           />
@@ -59,9 +84,10 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
+
       <div>
         <h2>Numbers</h2>
-        {persons.map((person, i) => 
+        {filterResult.map((person, i) => 
           <div key={person.name + i}>
             {person.name} {person.number}
           </div>

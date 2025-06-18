fbaf1f0b1522509bb3809e1f5c4df21ee696d843
Erudite <92492556+Erualpha@users.noreply.github.com>
2025-06-18 16:06:47 +0800

    Solution part3/phonebook-backend/step11

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -5,6 +5,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 
 morgan.token('post-body', (req, res) => {
   return req.method === 'POST' ? JSON.stringify(req.body) : ''
@@ -92,7 +93,7 @@ app.post('/api/persons', (request, response) => {
   response.status(201).json(newPerson)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })

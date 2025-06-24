275ba63098dff41b2452cf06b2dae7cd32ee0d1f
Erudite <92492556+Erualpha@users.noreply.github.com>
2025-06-24 12:04:13 +0800

    Solution step3/phonebook-databse/step4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -27,27 +27,31 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/info', (request, response) => {
-    const count = persons.length
-    const time = new Date()
-    response.send(`
+app.get('/info', (request, response, next) => {
+  Person.countDocuments({})
+    .then(count => {
+      const time = new Date()
+      response.send(`
         <p>Phonebook has info for ${count} people</p>
         <p>${time}</p>
-        `)
+      `)
+    })
+    .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = persons.find(person => person.id === id)
-
-    if (person) {
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
         response.json(person)
-    } else {
-        response.status(404).end()
-    }
+      } else {
+        response.status(404).end() 
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then(result => {
       response.status(204).end()
@@ -74,6 +78,20 @@ app.post('/api/persons', (request, response, next) => {
     })
     .catch(error => next(error))
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.name, error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {

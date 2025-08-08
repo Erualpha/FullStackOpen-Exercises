ef0afafab78a145f1296e4745608b4b381326e1e
Erudite <92492556+Erualpha@users.noreply.github.com>
2025-08-08 18:17:23 +0800

    Added solutions part4

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
+
 
 const app = express()
 
@@ -13,8 +18,13 @@ mongoose.connect(config.MONGODB_URI)
   })
 
 app.use(express.json())
+app.use(middleware.tokenExtractor)
 
-const blogsRouter = require('./controllers/blogs')
+
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+app.use(middleware.tokenExtractor)
+app.use('/api/login', loginRouter)
 
 module.exports = app
--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,14 +1,40 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
+  if (!request.token) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
+  let decodedToken
+  try {
+    decodedToken = jwt.verify(request.token, process.env.SECRET)
+  } catch (error) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
+
+  const blog = new Blog({
+    ...request.body,
+    user: user._id
+  })
   const savedBlog = await blog.save()
+
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   response.status(201).json(savedBlog)
 })
 
